b9022357743c57d42a3dbf27b6c642f2e5c9ec9a
harper71 <princeazuka99@gmail.com>
2024-06-29 00:34:44 +0100

    commit changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 # installs flasks using pip
+package { 'python':
+  ensure   => '3.8.10',
+  provider => 'apt',
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+package { 'werkzeug':
+  ensure   => '2.1.1'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
